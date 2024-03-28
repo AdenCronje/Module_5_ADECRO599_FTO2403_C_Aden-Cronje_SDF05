fa5bfc16dbbea20f256ccf0be019c477d7aac8c8
Aden Cronje <adencronje05@gmail.com>
2024-03-28 13:30:47 +0200

    Added array for cards

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 // Cards less than, more than or equal to 21
 let firstCard = 11;
 let secondCard = 5;
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 
 let isAlive = true;
 let message = "";
 let messageEl = document.getElementById("message-el");
-console.log(messageEl);
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
 function startGame() {
+  renderGame();
+}
+
+function renderGame() {
+  cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+  sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
-    message = "Do you want to draw a new card?🤔";
+    message = "Do you want to draw a new card?";
   } else if (sum === 21) {
-    message = "Congrats you've got BlackJack!😁";
+    message = "Congrats you've got BlackJack!";
     hasBlackJack = true;
   } else {
     isAlive = false;
-    message = "You're out of the game.😭";
+    message = "You're out of the game.";
   }
-  messageEl.textContent = message
+  messageEl.textContent = message;
+}
+
+function newCard() {
+  console.log("Drawing a new card from the deck!");
+  let card = 6;
+  sum += card;
+  cards.push(card);
+  console.log(cards);
+  renderGame();
 }
